3c38b1b5bd16e1246f32421915fb3971361ed546
realAnkis <david.lidar@yahoo.com>
2026-01-13 11:55:03 +0100

    kom på att man kan extenda en annan sak än jag gjorde så nu ser det lite finare ut

--- src/BlackjackMedKlasser/playMethods/SemiOptimalSubclasses/SimulatedRound.java
+++ src/BlackjackMedKlasser/playMethods/SemiOptimalSubclasses/SimulatedRound.java
@@ -5,14 +5,11 @@ import BlackjackMedKlasser.Settings;
 import BlackjackMedKlasser.playMethods.PlayMethod;
 import BlackjackMedKlasser.playMethods.SemiOptimal;
 
-public class SimulatedRound extends PlayMethod {
+public class SimulatedRound extends SemiOptimal {
     private Settings settings;
-    private SemiOptimal parentPlayMethod;
 
-
-    public SimulatedRound(Settings settings, SemiOptimal parentPlayMethod) {
-        this.settings = settings;
-        this.parentPlayMethod = parentPlayMethod;
+    public SimulatedRound(Settings settings) {
+        super(settings);
     }
 
     //körs när det ursprungliga bettet ska bestämmas
@@ -21,18 +18,10 @@ public class SimulatedRound extends PlayMethod {
         return settings.getMinBet();
     }
 
-    //körs när en aktion i spelet behöver bestämmas, bör returna "s", "h", "d" eller "sp"
-    //allowedActions har värdet 3 ifall alla är tillåtna, 2 ifall "h, s or d" är tillåtet och 1 om bara "h or s" är tillåtet
-    //handIndex är den hand som just nu spelas, bör användas i till exempel: round.getHands()[handIndex].getTotal()
+    /*
     @Override
     public String actionMethod(Round round, int allowedActions, int handIndex) {
         return parentPlayMethod.actionMethod(round,allowedActions,handIndex);
     }
-
-    //körs ifall möjlighet för ett insurance bet finns (dvs. ifall dealern har ett ess)
-    @Override
-    public int insuranceBetMethod(Round round) {
-        return 0;
-    }
-
+    */
 }
