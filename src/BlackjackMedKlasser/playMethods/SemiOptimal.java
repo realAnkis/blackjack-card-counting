50ea65211b6e69336a8be55869c232e945f315e4
realAnkis <david.lidar@yahoo.com>
2026-01-19 01:55:22 +0100

    nästan klar med en första version av semioptimal spelmetod, har dock en bugg som loopar oändligt

--- src/BlackjackMedKlasser/playMethods/SemiOptimal.java
+++ src/BlackjackMedKlasser/playMethods/SemiOptimal.java
@@ -3,21 +3,35 @@ package BlackjackMedKlasser.playMethods;
 import BlackjackMedKlasser.*;
 import BlackjackMedKlasser.playMethods.SemiOptimalSubclasses.SimulatedRound;
 
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class SemiOptimal extends PlayMethod {
     private final int betSimulationAmount = 1000;
     private final int actionSimulationAmount = 100;
-    private final int actionDepthSimulationAmount = 20;
+    private final int actionDepthSimulationAmount = 1;
+    private int looping;
 
     private Settings settings;
     private Deck predictedGameDeck;
+    private Deck betDeck;
     private Deck simulatedDeck;
     private Round simulatedRound;
+    private Hand simulatedDealerHand;
+    private Hand simulatedPlayerHand;
 
     public SemiOptimal(Settings settings) {
         this.settings = settings;
+        betDeck = new Deck(settings);
         simulatedDeck = new Deck(settings);
         predictedGameDeck = new Deck(settings);
-        simulatedRound = new Round(simulatedDeck,new SimulatedRound(settings),new Game());
+        if(!isSimulated()) simulatedRound = new Round(betDeck, new SimulatedRound(settings), new Game());
+        simulatedDealerHand = new Hand();
+        simulatedPlayerHand = new Hand();
+    }
+
+    public boolean isSimulated() {
+        return false;
     }
 
     //körs när ett kort delas ut från kortleken
@@ -37,17 +51,102 @@ public class SemiOptimal extends PlayMethod {
     //handIndex är den hand som just nu spelas, bör användas i till exempel: round.getHands()[handIndex].getTotal()
     @Override
     public String actionMethod(Round round, int allowedActions, int handIndex) {
-        int simulatedHitWinnings = 0;
-        for (int i = 0; i < actionSimulationAmount; i++) {
+        int[] winnings = tryActions(round, allowedActions, actionSimulationAmount, new LinkedList<>(round.getHands()[handIndex].getCards()));
 
-        }
+        String[] actions = new String[]{"s","h","d","sp"};
 
-        if(round.getHands()[handIndex].getTotal() <= 10) return "h";
-        return "s";
+        return actions[indexWithHighestValue(winnings)];
     }
 
-    public int tryActions() {
+    public int[] tryActions(Round round, int allowedActions, int iterationsPerAction, LinkedList<Card> startingCards) {
+        prepareForAction(startingCards);
 
+
+        int[] winnings = new int[4]; //index 0 = stand, 1 = hit, 2 = double, 3 = split
+
+        for (int i = 0; i < iterationsPerAction; i++) {
+            prepareForAction(startingCards);
+
+            winnings[0] += playSimulatedDealerHand(round, iterationsPerAction);
+        }
+        for (int i = 0; i < iterationsPerAction; i++) {
+            prepareForAction(startingCards);
+
+            simulatedPlayerHand.addCard(simulatedDeck.deal(this, false));
+            System.out.println(looping++);
+            int[] hitWinnings = tryActions(round, 1, actionDepthSimulationAmount, new LinkedList<>(simulatedPlayerHand.getCards()));
+
+            winnings[1] += iterationsPerAction * hitWinnings[indexWithHighestValue(hitWinnings)] / actionDepthSimulationAmount;
+        }
+        if(allowedActions == 1) {
+            winnings[2] = -100000000;
+            winnings[3] = -100000000;
+            return winnings;
+        }
+        for (int i = 0; i < iterationsPerAction; i++) {
+            prepareForAction(startingCards);
+            if(simulatedPlayerHand.getTotal() > 21) return new int[]{0,0,0,0};
+
+            simulatedPlayerHand.addCard(simulatedDeck.deal(this, false));
+
+            winnings[2] += playSimulatedDealerHand(round, 2 * iterationsPerAction);
+        }
+        if(allowedActions == 2) {
+            winnings[3] = -100000000;
+            return winnings;
+        }
+        for (int i = 0; i < iterationsPerAction; i++) {
+            prepareForAction(startingCards);
+
+            simulatedPlayerHand.pollCard();
+
+            int[] splitWinnings = tryActions(round, 2, actionDepthSimulationAmount, new LinkedList<>(simulatedPlayerHand.getCards()));
+
+            winnings[3] += 2 * iterationsPerAction * splitWinnings[indexWithHighestValue(splitWinnings)] / actionDepthSimulationAmount;
+        }
+        return winnings;
+    }
+
+    public int indexWithHighestValue(int[] array) {
+        int highestIndex = 0;
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > array[highestIndex]) highestIndex = i;
+        }
+
+        return highestIndex;
+    }
+
+    public int playSimulatedDealerHand(Round round, int betMultiplier) {
+        //plays the dealer hand and then returns winnings for given hand
+        simulatedDealerHand.clear();
+        simulatedDealerHand.addCard(round.getDealerHand().getCards().getFirst());
+
+        simulatedDealerHand.addCard(simulatedDeck.deal(this, false));
+
+        //dealer blackjack or hand busted
+        if (simulatedDealerHand.getTotal() == 21 || simulatedPlayerHand.getTotal() > 21) return 0;
+        //player blackjack
+        if (simulatedPlayerHand.getTotal() == 21 && simulatedPlayerHand.getCards().size() == 2)
+            return 5 * betMultiplier;
+
+        //deal dealer cards
+        while (simulatedDealerHand.getTotal() < 17) {
+            simulatedDealerHand.addCard(simulatedDeck.deal(this, false));
+        }
+        //if dealer busted or player has higher total
+        if (simulatedDealerHand.getTotal() > 21 || simulatedDealerHand.getTotal() < simulatedPlayerHand.getTotal())
+            return 4 * betMultiplier;
+        return 0;
+    }
+
+    public void prepareForAction(LinkedList<Card> startingCards) {
+        //set starting cards
+        simulatedPlayerHand.getCards().clear();
+        simulatedPlayerHand.getCards().addAll(startingCards);
+        //reset simulated deck and reshuffle the containing cards
+        simulatedDeck.setCards(predictedGameDeck.getCards());
+        Collections.shuffle(predictedGameDeck.getCards());
     }
 
     //körs när det ursprungliga bettet ska bestämmas
@@ -55,12 +154,12 @@ public class SemiOptimal extends PlayMethod {
     public int betMethod(Round round) {
         int simulatedWinnings = 0;
         for (int i = 0; i < betSimulationAmount; i++) {
-            simulatedDeck.setCards(predictedGameDeck.getCards());
-            simulatedDeck.shuffle();
+            betDeck.setCards(predictedGameDeck.getCards());
+            betDeck.shuffle();
             simulatedWinnings += simulatedRound.playRound();
             simulatedRound.reset();
         }
-        if(simulatedWinnings > 0) return settings.getMaxBet();
+        if (simulatedWinnings > 0) return settings.getMaxBet();
         return settings.getMinBet();
     }
 
@@ -68,10 +167,10 @@ public class SemiOptimal extends PlayMethod {
     @Override
     public int insuranceBetMethod(Round round) {
         int cardsWithValue10 = 0;
-        for(Card card : predictedGameDeck.getCards()) {
-            if(card.getValue() == 10) cardsWithValue10++;
+        for (Card card : predictedGameDeck.getCards()) {
+            if (card.getValue() == 10) cardsWithValue10++;
         }
-        if((double)(cardsWithValue10) / predictedGameDeck.getSizeOfDeck() > 0.5) return settings.getMaxBet();
+        if ((double) (cardsWithValue10) / predictedGameDeck.getSizeOfDeck() > 0.5) return settings.getMaxBet();
         return 0;
     }
 
--- src/BlackjackMedKlasser/playMethods/SemiOptimalSubclasses/SimulatedRound.java
+++ src/BlackjackMedKlasser/playMethods/SemiOptimalSubclasses/SimulatedRound.java
@@ -2,14 +2,21 @@ package BlackjackMedKlasser.playMethods.SemiOptimalSubclasses;
 
 import BlackjackMedKlasser.Round;
 import BlackjackMedKlasser.Settings;
-import BlackjackMedKlasser.playMethods.PlayMethod;
 import BlackjackMedKlasser.playMethods.SemiOptimal;
 
 public class SimulatedRound extends SemiOptimal {
     private Settings settings;
+    private final int actionSimulationAmount = 100;
+    private final int actionDepthSimulationAmount = 20;
 
     public SimulatedRound(Settings settings) {
         super(settings);
+        this.settings = settings;
+    }
+
+    @Override
+    public boolean isSimulated() {
+        return true;
     }
 
     //körs när det ursprungliga bettet ska bestämmas
